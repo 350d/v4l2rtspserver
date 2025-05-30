7f98607b04465e9977791a5e453f8d6c01b7097c
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-30 10:02:16 +0300

    Fix variable scope error in createH264Snapshot and header paths - Fix dataSize was not declared in scope error in createH264Snapshot function - Replace dataSize with h264Size parameter in 3 locations - Fix include paths for SnapshotManager.h and logger.h - Ensure consistent variable naming across MP4 creation functions

--- src/SnapshotManager.cpp
+++ src/SnapshotManager.cpp
@@ -9,8 +9,8 @@
 **
 ** -------------------------------------------------------------------------*/
 
-#include "SnapshotManager.h"
-#include "logger.h"
+#include "../inc/SnapshotManager.h"
+#include "../libv4l2cpp/inc/logger.h"
 #include "../libv4l2cpp/inc/V4l2Capture.h"
 #include <string>
 #include <vector>
@@ -550,8 +550,8 @@ void SnapshotManager::createH264Snapshot(const unsigned char* h264Data, size_t h
     }
     
     // H264 frame
-    write32(mdat, dataSize);
-    mdat.insert(mdat.end(), h264Data, h264Data + dataSize);
+    write32(mdat, h264Size);
+    mdat.insert(mdat.end(), h264Data, h264Data + h264Size);
     
     // Update mdat size
     uint32_t mdatSize = mdat.size();
@@ -807,7 +807,7 @@ void SnapshotManager::createH264Snapshot(const unsigned char* h264Data, size_t h
     stsz.insert(stsz.end(), {'s', 't', 's', 'z'});
     write8(stsz, 0); // version
     write8(stsz, 0); write8(stsz, 0); write8(stsz, 0); // flags
-    write32(stsz, sps.size() + 4 + pps.size() + 4 + dataSize + 4); // sample_size
+    write32(stsz, sps.size() + 4 + pps.size() + 4 + h264Size + 4); // sample_size
     write32(stsz, 1); // sample_count
     
     stbl.insert(stbl.end(), stsz.begin(), stsz.end());
@@ -877,7 +877,7 @@ void SnapshotManager::createH264Snapshot(const unsigned char* h264Data, size_t h
         m_lastSnapshotTimePoint = std::chrono::steady_clock::now();
         
         // Cache the frame data and SPS/PPS for future use
-        m_lastH264Frame.assign(h264Data, h264Data + dataSize);
+        m_lastH264Frame.assign(h264Data, h264Data + h264Size);
         if (!sps.empty()) m_lastSPS = sps;
         if (!pps.empty()) m_lastPPS = pps;
         m_lastFrameWidth = width;
